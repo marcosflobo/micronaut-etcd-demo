8e7e97b3ce68c88903d4a2d6ee67f2f2bad3b21a
Marcos F. Lobo <hello@marcosflobo.com>
2020-11-24 15:43:28 +0100

    Initial code commit

---
+++ src/test/java/com/marcosflobo/micronaut/etcd/demo/MicronautEtcdDemoTest.java
@@ -0,0 +1,21 @@
+package com.marcosflobo.micronaut.etcd.demo;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+public class MicronautEtcdDemoTest {
+
+    @Inject
+    EmbeddedApplication application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
